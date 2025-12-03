ce1290097da4b3beddff7a6726caa402715cfaef
pawsitiwe <info@pawsitiwe.de>
2025-12-03 06:41:04 +0100

    Import YamlFileLoader and FileLocator; add load(): void

--- src/DependencyInjection/PawsitiweSuluTurnstileExtension.php
+++ src/DependencyInjection/PawsitiweSuluTurnstileExtension.php
@@ -3,15 +3,17 @@
 namespace Pawsitiwe\SuluTurnstileBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 
 class PawsitiweSuluTurnstileExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new \Symfony\Component\DependencyInjection\Loader\YamlFileLoader(
+        $loader = new YamlFileLoader(
             $container,
-            new \Symfony\Component\Config\FileLocator(__DIR__.'/../../Resources/config')
+            new FileLocator(__DIR__.'/../../Resources/config')
         );
 
         $loader->load('services.yaml');
